2e32452b39f2d9a426cc0c37f00c5ab2682c6cf3
Guadalupe Pluma <moraplumanaidelynguadalupe@gmail.com>
2026-03-03 13:11:15 -0600

    agregar archivo

---
+++ vista/modulos/cabeceraUsuario.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+     <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container">
+            <img class="logo" src="img/logo.jpeg" alt="logo">
+
+            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Reportes</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Alertas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cerrar sesión</a>
+                    </li>
+
+                  
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
