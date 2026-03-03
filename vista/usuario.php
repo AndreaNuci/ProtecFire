9636998d8fb61d5c1e017ec5887fa8ea4d879e68
Guadalupe Pluma <moraplumanaidelynguadalupe@gmail.com>
2026-03-03 13:11:15 -0600

    agregar archivo

--- vista/usuario.php
+++ vista/usuario.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="vista/css/index.css">
 </head>
 <body>
-    <h1>vista de usuario</h1>
+     <?php
+    include "modulos/cabeceraUsuario.php"; ?>
 </body>
 </html>
