724b10b99084b64f6670e7f23a4e813704337fb7
aeromecanico15qoutlook.com <aeromecanico15@outlook.com>
2026-03-06 18:38:00 -0600

    Configuracion proyecto PHP con Docker y conexion a MySQL

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,9 +1,12 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "incendios");
+// Conexión original para entorno local (XAMPP)
+//$conexion = new mysqli("localhost", "root", "", "incendios");
+// Conexión para entorno Docker
+$conexion = new mysqli("db", "root", "root", "incendios");
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 
 $conexion->set_charset("utf8");
-?>
+/*?>*/
